f0a322882fd03fc25e44853662d76fd71c9bdc58
Leonardone <neetsdkasu@gmail.com>
2020-12-13 04:00:48 +0900

    initial commit

---
+++ src/GraphMIDlet.java
@@ -0,0 +1,22 @@
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+
+public final class GraphMIDlet extends MIDlet implements CommandListener
+{
+
+    protected void destroyApp(boolean unconditional) throws MIDletStateChangeException
+    {
+    }
+
+    protected void pauseApp()
+    {
+    }
+
+    protected void startApp() throws MIDletStateChangeException
+    {
+    }
+
+	public void commandAction(Command cmd, Displayable disp)
+	{
+    }
+}
