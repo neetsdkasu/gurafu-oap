25c18b4418ebc71ffe61604e514f881934f19cbd
Leonardone <neetsdkasu@gmail.com>
2020-12-13 04:17:52 +0900

    テンプレレベルを入れた

--- src/GraphMIDlet.java
+++ src/GraphMIDlet.java
@@ -3,6 +3,22 @@ import javax.microedition.midlet.*;
 
 public final class GraphMIDlet extends MIDlet implements CommandListener
 {
+    private static MainDisp mainDisp = null;
+
+    private static Command
+        exitCommand = null;
+
+    public GraphMIDlet()
+    {
+        mainDisp = new MainDisp();
+
+        exitCommand = new Command("EXIT", Command.EXIT, 1);
+        mainDisp.addCommand(exitCommand);
+
+        mainDisp.setCommandListener(this);
+
+        Display.getDisplay(this).setCurrent(mainDisp);
+    }
 
     protected void destroyApp(boolean unconditional) throws MIDletStateChangeException
     {
@@ -18,5 +34,9 @@ public final class GraphMIDlet extends MIDlet implements CommandListener
 
 	public void commandAction(Command cmd, Displayable disp)
 	{
+        if (cmd == exitCommand)
+        {
+            notifyDestroyed();
+        }
     }
 }
---
+++ src/MainDisp.java
@@ -0,0 +1,12 @@
+import javax.microedition.lcdui.*;
+import javax.microedition.lcdui.game.*;
+
+public final class MainDisp extends GameCanvas
+{
+
+    MainDisp()
+    {
+        super(false);
+    }
+
+}
